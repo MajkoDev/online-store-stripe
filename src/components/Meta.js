654a16a51a737355b57401f6b7dc77f1028c297f
Marian <hellomariandev@gmail.com>
2024-01-02 14:23:38 +0100

    layout

---
+++ src/components/Meta.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+export default function Meta() {
+  return (
+    <Head>
+      <title>Online Store</title>
+      <link rel="icon" type="image/x-icon" href="/favicon.ico?v=1" />
+    </Head>
+  );
+}
